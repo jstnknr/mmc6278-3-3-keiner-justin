d135e6f86d1402a4b02f7ae177e366fdc8d34c02
jstnknr <143372221+jstnknr@users.noreply.github.com>
2024-06-02 21:04:11 -0400

    adjust to make sure the scripts connect html

--- app.js
+++ app.js
@@ -1,35 +1,49 @@
-require('dotenv').config()
-const express = require('express')
-
-const app = express()
-// TODO: import the getCityInfo and getJobs functions from util.js
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
 const { getJobs, getCityInfo } = require('./util');
-// TODO: Statically serve the public folder
 
-// TODO: declare the GET route /api/city/:city
-// This endpoint should call getCityInfo and getJobs and return
-// the result as JSON.
-// The returned JSON object should have two keys:
-// cityInfo (with value of the getCityInfo function)
-// jobs (with value of the getJobs function)
-// If no city info or jobs are found,
-// the endpoint should return a 404 status
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/api/city/:city', async (req, res) => {
+  const { city } = req.params;
+  if (!city) return res.status(400).send({ error: "City is required" });
+
+  const cityInfo = await getCityInfo(city);
+  const jobs = await getJobs(city);
+
+  if (cityInfo && jobs) {
+    return res.status(200).json({ cityInfo, jobs });
+  }
+
+  return res.status(404).send({ error: "City info or jobs not found" });
+});
+
 app.get('/api/jobs', async (req, res) => {
-    const { location } = req.query;
-    if (!location) return res.status(400).send({ error: "Location is required" });
-  
-    const jobs = await getJobs(location);
-    if (jobs) return res.status(200).json(jobs);
-    return res.status(404).send({ error: "No jobs found" });
-  });
-  
-  app.get('/api/cityinfo', async (req, res) => {
-    const { location } = req.query;
-    if (!location) return res.status(400).send({ error: "Location is required" });
-  
-    const cityInfo = await getCityInfo(location);
-    if (cityInfo) return res.status(200).json(cityInfo);
-    return res.status(404).send({ error: "City info not found" });
-  });
-  
-module.exports = app
+  const { location } = req.query;
+  if (!location) return res.status(400).send({ error: "Location is required" });
+
+  const jobs = await getJobs(location);
+  if (jobs) return res.status(200).json(jobs);
+  return res.status(404).send({ error: "No jobs found" });
+});
+
+
+app.get('/api/cityinfo', async (req, res) => {
+  const { location } = req.query;
+  if (!location) return res.status(400).send({ error: "Location is required" });
+
+  const cityInfo = await getCityInfo(location);
+  if (cityInfo) return res.status(200).json(cityInfo);
+  return res.status(404).send({ error: "City info not found" });
+});
+
+module.exports = app;
